519b0b57d082797c92cca247c823c89027ff0a01
fkundih21 <fkundih21@student.foi.hr>
2025-03-18 22:02:04 +0100

    - Implemented authentication logic - Added user authentication endpoints

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController
+{
+    public function register(Request $request)
+    {
+
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create($fields);
+        $token = $user->createToken($request->name);
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+        //return response()->json(['message' => 'User registered successfully','user' => $user, 'token' => $token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return[
+                'message' => 'The provided credentials are incorrect.'
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return[
+            'message' => 'You are logged out'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,30 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 
-Route::middleware('api')->group(function () {
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware([
+    'api',
+    EnsureFrontendRequestsAreStateful::class,
+])->group(function () {
     //GET ALL TASKS
     Route::get('/tasks', [TaskController::class, 'index']);
     //GET ALL TASKS BY SPECIFIC USER
     Route::get('/users/{user_id}/tasks', [TaskController::class, 'getTasksByUser']);
     //GET TASK BY ID
-    Route::get('/tasks/{id}', [TaskController::class, 'show']);
+    Route::get('/tasks/{id}', [TaskController::class, 'show'])->middleware('auth:sanctum');
     //POST NEW TASK
-    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::post('/tasks', [TaskController::class, 'store'])->middleware('auth:sanctum');
     //PUT UPDATE TASK
-    Route::put('/tasks/{id}', [TaskController::class, 'update']);
+    Route::put('/tasks/{id}', [TaskController::class, 'update'])->middleware('auth:sanctum');
     //DELETE TASK BY ID
-    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->middleware('auth:sanctum');
 });
